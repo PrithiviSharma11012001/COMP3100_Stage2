957a2bc7bd1634f392e655a431e92623bab295a0
Prithivi Sharma <prithivi.sharma@students.mq.edu.au>
2021-05-28 20:12:39 +1000

    Added all files and made changes to stage 1 code for stage 2
    Added all the files that were used for stage 1 along with the test file used for stage 2. Created two new java files for stage 2 and wrote code to break a job information.

---
+++ src/pre-compiled/TheClient.java
@@ -0,0 +1,200 @@
+import java.net.*;
+import java.io.*;
+import org.w3c.dom.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.ArrayList;
+
+public class TheClient {
+
+	// Create global variables for the socket, and the input and output, which we will need to write and read messages
+	// between server and client
+	private Socket socket = null;
+	private BufferedReader in = null;
+	private DataOutputStream out = null;
+	private Server[] servers = new Server[1];
+	private ArrayList<Server> servers_array_list = new ArrayList<Server>(); 
+	private int largestServerIndex = 0;
+	private String inputString;
+	private Boolean completed = false;
+
+	// Constructor for our client class: we connect the socket to the address 127.0.0.1 and to the port 50000, as
+	// provided by the server, and we initialize the input variable (in) and the output (out)
+	// in will be used for reading the messages sent by the server, while out will be used for writing messages 
+	// to the server
+	public TheClient() {
+		try {
+			socket = new Socket("localhost", 50000);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new DataOutputStream(socket.getOutputStream()); 
+		} catch (UnknownHostException i) {
+			System.out.println("Error: " + i);
+		} catch (IOException i) {
+			System.out.println("Error: " + i);
+		}
+	}
+    
+	// We instantiate one client object in main, and we need some method that will start communication with the server,
+	// which I called start().
+	public static void main(String args[]) {
+		TheClient client = new TheClient();
+		client.start();
+	}
+
+	public void start() {
+		write("HELO");
+        //System.out.println("Sent HELLO");
+		inputString = read();
+        //System.out.println("Received " + inputString);
+		write("AUTH " + System.getProperty("user.name"));
+        //System.out.println("Sent Auth " + System.getProperty("user.name"));
+		inputString = read();
+        //System.out.println("Received " + inputString);
+		File file = new File("ds-system.xml");
+		readFile(file);
+		write("REDY");
+        //System.out.println("Sent REDY");
+		inputString = read();
+        //System.out.println("Received " + inputString);
+        allToLargest();
+		quit();
+	}
+
+	// allToLargest deals with the communication between server and client after the client sent the first REDY, and
+	// the server sent the first reply. If the reply is NONE, the method will just quit and the connection will be closed,
+	// while, if the client receives other commands, the algorithm will run as expected. It follows the workflow shown in
+	// the ds-server pdf file provided, so the algorithm loops until it receives a NONE as a reply from the server. 
+	// In case it does not receive a JOBN reply, the client will write REDY, so that the command can be skipped.
+	// When the server sends a JOBN message, the message is split and data about the job is gathered.
+	// The server type needed in the SCHD command is found thanks to the readFile method and the findLargestServer method.
+	public void allToLargest (){
+        if (inputString.equals("NONE")) {
+			quit();
+		} else {
+			while (!completed) {
+				if (inputString.equals("OK")) {
+					write("REDY");
+                    //System.out.println("Sent REDY");
+					inputString = read();
+                    //System.out.println("Received " + inputString);
+				}
+                String [] splitMessage = inputString.split("\\s+");
+                String firstWord = splitMessage[0];
+                while (firstWord.equals("JCPL") || firstWord.equals("RESF") || firstWord.equals("RESR")) {
+                    write("REDY");
+                    //System.out.println("Sent REDY");
+					inputString = read();
+                    //System.out.println("Received " + inputString);
+
+                    splitMessage = inputString.split("\\s+");
+                    firstWord = splitMessage[0];
+                }
+				if (firstWord.equals("NONE")) {
+					completed = true;
+					break;
+				}
+
+				String[] new_job = inputString.split("\\s+"); 
+				//In the line below, the given job is broken into smaller areas of information to create an object of given_job
+				given_job dissected_job = new given_job(Integer.parseInt(new_job[1]), Integer.parseInt(new_job[2]),
+				Integer.parseInt(new_job[3]), Integer.parseInt(new_job[4]), Integer.parseInt(new_job[5]),
+				Integer.parseInt(new_job[6]));
+
+				//Every piece of information regarding the job is obtained.
+				write("GETS All");
+				//String num = jobSections[2];
+				//String scheduleMessage = "SCHD " + num + " " + servers[largestServerIndex].type + " " + "0";
+				//write(scheduleMessage);
+                //System.out.println("JOB SENT: SCHD " + count + " " + servers[largestServer].type + " " + "0");
+                inputString = read();
+				write("OK");
+                
+				inputString = read(); 
+
+				
+				servers_array_list = new ArrayList<Server>();
+
+
+
+			}
+		}
+    }
+
+	// This method parses through the XML file found at the path stated in the start() method. 
+	// It iterates through the file looks for attributes found in the XML file.
+	// It then stores those values in an array
+	public void readFile(File file) {
+		try {
+			
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document systemDocument = builder.parse(file);
+			systemDocument.getDocumentElement().normalize();
+			
+			NodeList serverNodeList = systemDocument.getElementsByTagName("server");
+			servers = new Server[serverNodeList.getLength()];
+			for (int i = 0; i < serverNodeList.getLength(); i++) {
+				Element server = (Element) serverNodeList.item(i);
+				String t = server.getAttribute("type");
+				int c = Integer.parseInt(server.getAttribute("coreCount"));
+				Server temp = new Server(i, t, c);
+				servers[i] = temp;
+			}
+			largestServerIndex = findLargestServer();
+		} catch (Exception i) {
+			i.printStackTrace();
+		}
+
+	}
+
+	// Returns the index of the largest server(CPU cores) in the array 
+	// created by the readFile() method
+	public int findLargestServer() {
+		int largestServer = servers[0].id;
+		for (int i = 0; i < servers.length; i++) {
+			if (servers[i].cores > servers[largestServer].cores) {
+				largestServer = servers[i].id;
+			}
+		}
+		return largestServer;
+	}
+
+	public void write(String text) {
+		try {
+			out.write((text + "\n").getBytes());
+			// System.out.print("SENT: " + text);
+			out.flush();
+		} catch (IOException i) {
+			System.out.println("ERR: " + i);
+		}
+	}
+
+	public String read() {
+		String text = "";
+		try {
+            text = in.readLine();
+			// System.out.print("RCVD: " + text);
+			inputString = text;
+		} catch (IOException i) {
+			System.out.println("ERR: " + i);
+		}
+		return text;
+	}
+
+	public void quit() {
+		try {
+			write("QUIT");
+            //System.out.println("Sent QUIT");
+			inputString = read();
+            //System.out.println("Received" + inputString);
+			if (inputString.equals("QUIT")) {
+				in.close();
+				out.close();
+				socket.close();
+			}
+		} catch (IOException i) {
+			System.out.println("ERR: " + i);
+		}
+	}
+
+}
---
+++ src/pre-compiled/given_job.java
@@ -0,0 +1,27 @@
+public class given_job{
+
+    public int submission_time; 
+    public int job_id; 
+    public int running_time_estimate; 
+    public int core; 
+    public int mem; 
+    public int disk; 
+
+
+
+
+
+    
+    given_job(int sub_time, int j_id,int rte, int core, int mem, int disk){
+
+        this.submission_time = sub_time; 
+        this.job_id = j_id; 
+        this.running_time_estimate = rte; 
+        this.core = core;
+        this.mem = mem; 
+        this.disk = disk; 
+
+    }
+
+
+}
